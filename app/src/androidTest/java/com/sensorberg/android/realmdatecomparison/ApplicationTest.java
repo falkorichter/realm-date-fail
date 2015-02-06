66227e2f7822a521ceb73c8247fa7984827ed72b
Falko Richter <falko@briefhansa.de>
2015-02-06 18:08:32 +0100

    another comparison

--- app/src/androidTest/java/com/sensorberg/android/realmdatecomparison/ApplicationTest.java
+++ app/src/androidTest/java/com/sensorberg/android/realmdatecomparison/ApplicationTest.java
@@ -20,6 +20,7 @@ public class ApplicationTest extends ApplicationTestCase<Application> {
     public void test_realm_date_comparison() throws Exception {
         Realm realm = Realm.getInstance(getContext(), String.valueOf(System.currentTimeMillis()));
         Date firstDate = new Date(0);
+        Date date2 = new Date(999);
 
         Assertions.assertThat(realm.where(RealmObjectWithDate.class).findAll()).hasSize(0);
 
@@ -28,10 +29,14 @@ public class ApplicationTest extends ApplicationTestCase<Application> {
 
         object1.setAnyDate(firstDate);
         RealmObjectWithDate object2 = realm.createObject(RealmObjectWithDate.class);
-        object2.setAnyDate(new Date(999));
+
+        object2.setAnyDate(date2);
         realm.commitTransaction();
 
         Assertions.assertThat(realm.where(RealmObjectWithDate.class).findAll()).hasSize(2);
+
+        //these two dates are not equal!
+        Assertions.assertThat(firstDate.compareTo(date2)).isNotEqualTo(0);
 
         //should only find object2
         Assertions.assertThat(realm.where(RealmObjectWithDate.class)
